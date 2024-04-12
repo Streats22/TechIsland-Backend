e1359916451ae98a2eeff71f2df2ea6ce259f0d9
Robin Schoenmaker <robin.schoenmaker@outlook.com>
2024-04-12 16:44:48 +0200

    installer removed

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['middleware' => 'redirect.if.not.installed'], function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
-});
