f7f358fbe4a2f0c4eb13e0ba5e5697e205c1ea1a
Robin Schoenmaker <robin.schoenmaker@outlook.com>
2024-05-29 13:55:54 +0200

    bug fix no page found

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,15 @@
 <?php
-
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 
 Route::get('/', function () {
-    return redirect('/admin/login');
+    return redirect('admin/login');
 });
-
-Route::get('/admin', function () {
-    return  redirect('/admin/login');
-});
-
-Route::get('/teacher', function () {
-    return redirect('/teacher/login');
-});
-
 
 
 // Password reset and other admin-specific routes
-Route::prefix('admin')->middleware(['auth:web'])->group(function () {
+Route::prefix('admin')->group(function () {
     Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('filament.password.reset');
     Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('filament.password.update');
     Route::get('password/request', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('filament.password.request');
@@ -36,7 +25,7 @@ Route::prefix('teacher')->middleware(['auth:teacher'])->group(function () {
     Route::get('/personal-profile', function () {
         logger('Personal profile route hit.');
         return app(\App\Filament\Resources\TeachersResource\Pages\PersonalProfile::class)->render();
-    })->name('filament.teacher.resources.teachers.profile');
+    })->name('teacher.personal-profile');
 
 });
 // Routes specific to deans
@@ -45,3 +34,4 @@ Route::prefix('dean')->middleware(['auth:dean'])->group(function () {
         return view('dean.dashboard');  // Assuming you have a dashboard view for deans
     })->name('dean.dashboard');
 });
+
